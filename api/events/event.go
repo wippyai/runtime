6982bdec4436879da36dda4809a1d483380be607
Wolfy-J <wolfy-j@spiralscout.com>
2024-12-12 23:57:41 -0500

    - history and registry - runner encapsulated now

--- api/events/event.go
+++ api/events/event.go
@@ -2,32 +2,38 @@ package events
 
 import (
 	"context"
-	"strings"
 )
 
 type (
+	// SubscriberID represents a unique identifier for a subscriber.
 	SubscriberID string
-	System       string
-	Kind         string
 
+	// System represents a system or module that events belong to (e.g., "config", "runtime", etc).
+	System string
+
+	// Kind represents the specific type of an event within a system (e.g., "config.created", "status.online").
+	Kind string
+
+	// Event is the fundamental structure representing an event.
 	Event struct {
+		// System is the system or module the event originates from.
 		System System
-		Kind   Kind
-		Data   any
+		// Kind is the specific type of the event.
+		Kind Kind
+		// Data is the payload of the event, which can be any relevant data associated with the event.
+		Data any
 	}
 
+	// Bus is an interface defining the core functionality of an event bus.
+	// It allows subscribers to listen for events and publishers to send events.
 	Bus interface {
+		// Subscribe subscribes a channel to events from a specific system.
 		Subscribe(context.Context, System, chan<- Event) (SubscriberID, error)
+		// SubscribeP subscribes a channel to events from a specific system and matching a specific pattern.
 		SubscribeP(context.Context, System, Kind, chan<- Event) (SubscriberID, error)
+		// Unsubscribe unsubscribes a subscriber using its SubscriberID.
 		Unsubscribe(context.Context, SubscriberID)
+		// Send publishes an event to the bus.
 		Send(context.Context, Event)
 	}
-
-	Consumer interface {
-		Register(context.Context, Event)
-	}
 )
-
-func NewKind(path ...string) Kind {
-	return Kind(strings.Join(path, "."))
-}
