ef6c708bd7f1cbd486520af4465abc5c794e6b75
Wolfy-J <wolfy-j@spiralscout.com>
2024-12-29 16:49:53 -0500

    - happy path is ready!

--- runtime/lua/compiler.go
+++ runtime/lua/compiler.go
@@ -3,6 +3,7 @@ package lua
 import (
 	"github.com/ponyruntime/pony/api/runtime/lua"
 	"github.com/ponyruntime/pony/runtime/lua/pool"
+	"github.com/ponyruntime/pony/runtime/lua/pool/queued"
 	"github.com/ponyruntime/pony/runtime/lua/pool/sync"
 	"go.uber.org/zap"
 )
@@ -21,6 +22,15 @@ func (c *Compiler) Compile(
 	vmCfg *pool.VMConfig,
 	luaCfg *lua.FunctionConfig,
 ) (lua.Callable, error) {
+	if luaCfg.Pool.Workers > 0 {
+		return queued.NewPool(
+			vmCfg,
+			queued.WithSize(luaCfg.Pool.Size),
+			queued.WithLogger(c.log),
+			queued.WithWorkers(luaCfg.Pool.Workers),
+		)
+	}
+
 	return sync.NewPool(
 		vmCfg,
 		sync.WithSize(luaCfg.Pool.Size),
