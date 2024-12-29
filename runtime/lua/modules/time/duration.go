6f83d9d6973240e0b171bd0fbccb4609fac46e69
Wolfy-J <wolfy-j@spiralscout.com>
2024-12-29 14:53:03 -0500

    - time package (we need it for testing) and its tests

---
+++ runtime/lua/modules/time/duration.go
@@ -0,0 +1,138 @@
+package time
+
+import (
+	"time"
+
+	"github.com/ponyruntime/go-lua"
+)
+
+// Module represents a time Lua module
+type Module struct{}
+
+// Duration represents a Lua userdata object wrapping time.Duration
+type Duration struct {
+	duration time.Duration
+}
+
+// Register duration methods
+var durationMethods = map[string]lua.LGFunction{
+	"nanoseconds":  durationNanoseconds,
+	"microseconds": durationMicroseconds,
+	"milliseconds": durationMilliseconds,
+	"seconds":      durationSeconds,
+	"minutes":      durationMinutes,
+	"hours":        durationHours,
+}
+
+// Duration methods implementations
+func durationNanoseconds(l *lua.LState) int {
+	ud := l.CheckUserData(1)
+	if d, ok := ud.Value.(*Duration); ok {
+		l.Push(lua.LNumber(d.duration.Nanoseconds()))
+		return 1
+	}
+	l.ArgError(1, "duration expected")
+	return 0
+}
+
+func durationMicroseconds(l *lua.LState) int {
+	ud := l.CheckUserData(1)
+	if d, ok := ud.Value.(*Duration); ok {
+		l.Push(lua.LNumber(d.duration.Microseconds()))
+		return 1
+	}
+	l.ArgError(1, "duration expected")
+	return 0
+}
+
+func durationMilliseconds(l *lua.LState) int {
+	ud := l.CheckUserData(1)
+	if d, ok := ud.Value.(*Duration); ok {
+		l.Push(lua.LNumber(d.duration.Milliseconds()))
+		return 1
+	}
+	l.ArgError(1, "duration expected")
+	return 0
+}
+
+func durationSeconds(l *lua.LState) int {
+	ud := l.CheckUserData(1)
+	if d, ok := ud.Value.(*Duration); ok {
+		l.Push(lua.LNumber(d.duration.Seconds()))
+		return 1
+	}
+	l.ArgError(1, "duration expected")
+	return 0
+}
+
+func durationMinutes(l *lua.LState) int {
+	ud := l.CheckUserData(1)
+	if d, ok := ud.Value.(*Duration); ok {
+		l.Push(lua.LNumber(d.duration.Minutes()))
+		return 1
+	}
+	l.ArgError(1, "duration expected")
+	return 0
+}
+
+func durationHours(l *lua.LState) int {
+	ud := l.CheckUserData(1)
+	if d, ok := ud.Value.(*Duration); ok {
+		l.Push(lua.LNumber(d.duration.Hours()))
+		return 1
+	}
+	l.ArgError(1, "duration expected")
+	return 0
+}
+
+// durationToString implements the __tostring metamethod for Duration
+func durationToString(l *lua.LState) int {
+	ud := l.CheckUserData(1)
+	if d, ok := ud.Value.(*Duration); ok {
+		l.Push(lua.LString(d.duration.String()))
+		return 1
+	}
+	l.ArgError(1, "duration expected")
+	return 0
+}
+
+// parseDuration implements time.ParseDuration for Lua
+func parseDuration(l *lua.LState) int {
+	str := l.CheckString(1)
+
+	if str == "" {
+		l.ArgError(1, "empty string")
+		return 0
+	}
+
+	duration, err := time.ParseDuration(str)
+	if err != nil {
+		l.Push(lua.LNil)
+		l.Push(lua.LString(err.Error()))
+		return 2
+	}
+
+	ud := l.NewUserData()
+	ud.Value = &Duration{duration: duration}
+	l.SetMetatable(ud, l.GetTypeMetatable("Duration"))
+	l.Push(ud)
+	return 1
+}
+
+func registerDuration(l *lua.LState, mod *lua.LTable) {
+	mt := l.NewTypeMetatable("Duration")
+	l.SetField(mt, "__index", l.SetFuncs(l.NewTable(), durationMethods))
+	l.SetField(mt, "__tostring", l.NewFunction(durationToString))
+
+	// Register duration constants
+	l.SetField(mod, "NANOSECOND", lua.LNumber(Nanosecond))
+	l.SetField(mod, "MICROSECOND", lua.LNumber(Microsecond))
+	l.SetField(mod, "MILLISECOND", lua.LNumber(Millisecond))
+	l.SetField(mod, "SECOND", lua.LNumber(Second))
+	l.SetField(mod, "MINUTE", lua.LNumber(Minute))
+	l.SetField(mod, "HOUR", lua.LNumber(Hour))
+
+	l.SetFuncs(mod, map[string]lua.LGFunction{
+		"parse_duration": parseDuration,
+	})
+}
---
+++ runtime/lua/modules/time/location.go
@@ -0,0 +1,88 @@
+package time
+
+import (
+	"time"
+
+	"github.com/ponyruntime/go-lua"
+)
+
+// Location represents a Lua userdata object wrapping time.Location
+type Location struct {
+	location *time.Location
+}
+
+var locationMethods = map[string]lua.LGFunction{
+	"string": locationString,
+}
+
+// locationString returns the name of the location
+func locationString(l *lua.LState) int {
+	ud := l.CheckUserData(1)
+	if loc, ok := ud.Value.(*Location); ok {
+		l.Push(lua.LString(loc.location.String()))
+		return 1
+	}
+	l.ArgError(1, "location expected")
+	return 0
+}
+
+// loadLocation implements time.LoadLocation for Lua
+func loadLocation(l *lua.LState) int {
+	name := l.CheckString(1)
+
+	if name == "" {
+		l.Push(lua.LNil)
+		l.Push(lua.LString("empty location name"))
+		return 2
+	}
+
+	loc, err := time.LoadLocation(name)
+	if err != nil {
+		l.Push(lua.LNil)
+		l.Push(lua.LString(err.Error()))
+		return 2
+	}
+
+	ud := l.NewUserData()
+	ud.Value = &Location{location: loc}
+	l.SetMetatable(ud, l.GetTypeMetatable("Location"))
+	l.Push(ud)
+	return 1
+}
+
+// fixedZone implements time.FixedZone for Lua
+func fixedZone(l *lua.LState) int {
+	name := l.CheckString(1)
+	offset := l.CheckInt(2)
+
+	loc := time.FixedZone(name, offset)
+
+	ud := l.NewUserData()
+	ud.Value = &Location{location: loc}
+	l.SetMetatable(ud, l.GetTypeMetatable("Location"))
+	l.Push(ud)
+	return 1
+}
+
+// Register the Location methods and create UTC/Local constants
+func registerLocation(l *lua.LState, mod *lua.LTable) {
+	locationMt := l.NewTypeMetatable("Location")
+	l.SetField(locationMt, "__index", l.SetFuncs(l.NewTable(), locationMethods))
+	l.SetField(locationMt, "__tostring", l.NewFunction(locationString))
+
+	// Register location-related functions
+	l.SetField(mod, "load_location", l.NewFunction(loadLocation))
+	l.SetField(mod, "fixed_zone", l.NewFunction(fixedZone))
+
+	// Create and set UTC constant
+	utcUD := l.NewUserData()
+	utcUD.Value = &Location{location: time.UTC}
+	l.SetMetatable(utcUD, locationMt)
+	l.SetField(mod, "utc", utcUD)
+
+	// Create and set Local constant
+	localUD := l.NewUserData()
+	localUD.Value = &Location{location: time.Local}
+	l.SetMetatable(localUD, locationMt)
+	l.SetField(mod, "localtz", localUD)
+}
