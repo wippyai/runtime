1cc0b1eb40d0eac1552aa144f0fcd02f51bc9324
Wolfy-J <wolfy-j@spiralscout.com>
2025-01-01 13:51:49 -0500

    - refactoring in http module

--- __redo/sql/loader.go
+++ __redo/sql/loader.go
@@ -14,14 +14,14 @@ func (m *Module) Loader(l *lua.LState) int {
 	// generate the DB methods as a metatable
 	dbMt := l.NewTypeMetatable("DB")
 	l.SetField(dbMt, "__index", l.SetFuncs(l.NewTable(), map[string]lua.LGFunction{
-		"execute":          execute,
-		"query":            query,
-		"prepare":          prepare,
-		"executePrepared":  executePrepared,
-		"beginTransaction": beginTransaction,
-		"commit":           commit,
-		"rollback":         rollback,
-		"close":            closeDB,
+		"execute":           execute,
+		"query":             query,
+		"prepare":           prepare,
+		"execute_prepared":  executePrepared,
+		"begin_transaction": beginTransaction,
+		"commit":            commit,
+		"rollback":          rollback,
+		"close":             closeDB,
 	}))
 
 	// Push the SQL module table
