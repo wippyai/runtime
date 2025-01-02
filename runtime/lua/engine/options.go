23fb3805f8fada63a6415bbe3530f32a46aba60e
Wolfy-J <wolfy-j@spiralscout.com>
2025-01-02 09:21:05 -0500

    - minor cs and payload formatting

--- runtime/lua/engine/options.go
+++ runtime/lua/engine/options.go
@@ -3,15 +3,12 @@ package engine
 import (
 	"fmt"
 	"github.com/ponyruntime/go-lua"
-	"go.uber.org/zap"
 	"strings"
 )
 
 // WithLibrary adds a library from source code to the VM
 func WithLibrary(name string, source string) Option {
 	return func(vm *VM) {
-		vm.log.Debug("loading library from source", zap.String("name", name))
-
 		loader := func(L *lua.LState) int {
 			fn, err := L.Load(strings.NewReader(source), fmt.Sprintf("<%s>", name))
 			if err != nil {
@@ -46,21 +43,18 @@ func WithLibrary(name string, source string) Option {
 // WithLoader adds a library with a custom loader function to the VM
 func WithLoader(name string, loader lua.LGFunction) Option {
 	return func(vm *VM) {
-		vm.log.Debug("loading library with loader", zap.String("name", name))
 		vm.state.PreloadModule(name, loader)
 	}
 }
 
 func WithGlobalFunction(name string, function lua.LGFunction) Option {
 	return func(vm *VM) {
-		vm.log.Debug("setting global function", zap.String("name", name))
 		vm.state.SetGlobal(name, vm.state.NewFunction(function))
 	}
 }
 
 func WithGlobalValue(name string, value lua.LValue) Option {
 	return func(vm *VM) {
-		vm.log.Debug("setting global value", zap.String("name", name))
 		vm.state.SetGlobal(name, value)
 	}
 }
