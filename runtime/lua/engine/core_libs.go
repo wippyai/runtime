dbf8514c73ed803cba9a1ce007cec4cdbb49f436
Wolfy-J <wolfy-j@spiralscout.com>
2025-01-02 23:51:16 -0500

    - migration done

---
+++ runtime/lua/engine/core_libs.go
@@ -0,0 +1,51 @@
+package engine
+
+import (
+	"github.com/yuin/gopher-lua"
+)
+
+// CoreLib represents a core Lua library
+type CoreLib struct {
+	name string
+	fn   lua.LGFunction
+}
+
+// coreLuaLibs defines the core Lua libraries to load
+var coreLuaLibs = []CoreLib{
+	{lua.LoadLibName, lua.OpenPackage}, // Must be first
+	{lua.BaseLibName, lua.OpenBase},
+	{lua.TabLibName, lua.OpenTable},
+	{lua.StringLibName, lua.OpenString},
+	{lua.MathLibName, lua.OpenMath},
+	{lua.DebugLibName, lua.OpenDebug},
+	{lua.CoroutineLibName, lua.OpenCoroutine},
+	// never os or io
+}
+
+// loadCoreLuaLibs loads the core Lua libraries into the state
+func loadCoreLuaLibs(state *lua.LState) error {
+	for _, lib := range coreLuaLibs {
+		if err := state.CallByParam(lua.P{
+			Fn:      state.NewFunction(lib.fn),
+			NRet:    0,
+			Protect: true,
+		}, lua.LString(lib.name)); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// newLuaState creates a new Lua state with core libraries
+func newLuaState() (*lua.LState, error) {
+	state := lua.NewState(lua.Options{
+		SkipOpenLibs: true,
+	})
+
+	if err := loadCoreLuaLibs(state); err != nil {
+		state.Close()
+		return nil, err
+	}
+
+	return state, nil
+}
