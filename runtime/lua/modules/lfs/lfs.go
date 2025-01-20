07ee6102794f8e08e46a7c57d627ae5b32525942
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-01-20 13:11:55 +0100

    feature: EE2-1564 Make LFS context-specific
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- runtime/lua/modules/lfs/lfs.go
+++ runtime/lua/modules/lfs/lfs.go
@@ -4,15 +4,27 @@
 package lfs
 
 import (
+	"os"
+
 	lua "github.com/yuin/gopher-lua"
+	"go.uber.org/zap"
 )
 
 // Module represents a lfs Lua module.
-type Module struct{}
+type Module struct {
+	currDir string
+	log     *zap.Logger
+}
 
 // NewLFSModule creates and returns a new instance of the lfs Module.
-func NewLFSModule() *Module {
-	return &Module{}
+func NewLFSModule(log *zap.Logger) *Module {
+	// TODO: is it safe to omit error handling here?
+	// case1: cwd does not exist
+	dir, _ := os.Getwd()
+	return &Module{
+		currDir: dir,
+		log:     log,
+	}
 }
 
 // Name returns the module's name.
@@ -24,19 +36,21 @@ func (m *Module) Loader(l *lua.LState) int {
 	t := l.NewTable()
 
 	api := map[string]lua.LGFunction{
-		"attributes":        apiAttributes,
-		"chdir":             apiChdir,
-		"lock_dir":          apiLockdir,
-		"currentdir":        apiCurrentdir,
-		"dir":               apiDir,
-		"lock":              apiLock,
-		"link":              apiLink,
-		"mkdir":             apiMkdir,
-		"rmdir":             apiRmdir,
-		"setmode":           apiSetmode,
-		"symlinkattributes": apiSymlinkattributes,
-		"touch":             apiTouch,
-		"unlock":            apiUnlock,
+		"attributes":        m.apiAttributes,
+		"chdir":             m.apiChdir,
+		"lock_dir":          m.apiLockdir,
+		"currentdir":        m.apiCurrentdir,
+		"dir":               m.apiDir,
+		"lock":              m.apiLock,
+		"link":              m.apiLink,
+		"mkdir":             m.apiMkdir,
+		"rmdir":             m.apiRmdir,
+		"setmode":           m.apiSetmode,
+		"symlinkattributes": m.apiSymlinkattributes,
+		"touch":             m.apiTouch,
+		"unlock":            m.apiUnlock,
+		// to close a file in the userdata
+		"close": m.close,
 	}
 
 	l.SetFuncs(t, api)
