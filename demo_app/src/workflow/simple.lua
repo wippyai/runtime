7c7047d8d3c22d31f0e54c6e761a4f9ade5f6c55
Wolfy-J <wolfy.jd@gmail.com>
2025-01-30 18:38:24 -0500

    - hacking stuff together

--- demo_app/src/workflow/simple.lua
+++ demo_app/src/workflow/simple.lua
@@ -3,12 +3,18 @@ local wf = require "temporal_workflow"
 function execute_workflow()
     -- Create an activity command with required parameters
     local activity_cmd = command.new(
-        "activity",    -- command type
-        "hello-world", -- activity name
-        {              -- activity options
-            taskQueue = "default_tq",
-            startToCloseTimeout = "5s",
-            scheduleToCloseTimeout = "10s"
+        "activity",             -- command type
+        "hello_world.activity", -- activity name
+        {
+            task_queue = "wippy_demos",
+            start_to_close_timeout = "5s",
+            schedule_to_close_timeout = "10s",
+            retry_policy = {
+                initial_interval = "1s",
+                backoff_coefficient = 2.0,
+                maximum_interval = "100s",
+                maximum_attempts = 3
+            }
         },
         "Hello", "World" -- activity arguments
     )
