f4f5d2c29466793cc3018890dcf86c68bc97b10c
Wolfy-J <wolfy.jd@gmail.com>
2025-02-10 17:04:05 -0500

    - noop fix

--- runtime/noop/noop_test.go
+++ runtime/noop/noop_test.go
@@ -65,7 +65,7 @@ func TestNoopRuntime_Execute(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			resultCh, err := n.Execute(tt.task)
+			resultCh, err := n.Execute(context.Background(), tt.task)
 			if tt.wantErr {
 				require.Error(t, err)
 				return
