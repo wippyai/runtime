973d1b54156c6e3011fbe40aad3b3f44010c39a8
Wolfy-J <wolfy.jd@gmail.com>
2025-02-15 18:12:51 -0500

    - pubsub

---
+++ api/process/pubsub.go
@@ -0,0 +1,24 @@
+package process
+
+import (
+	"context"
+	"errors"
+	"github.com/ponyruntime/pony/api/payload"
+)
+
+var ErrAlreadyAttached = errors.New("receiver already attached")
+
+type (
+	Topic = string
+
+	Message struct {
+		Topic    Topic
+		Payloads payload.Payloads
+	}
+
+	PubSub interface {
+		Send(ctx context.Context, pid PID, msg ...*Message) error
+		Attach(PID, Receiver) error
+		Detach(PID) error
+	}
+)
