4dc70c600fb5c0b518437c99371eae22c923d119
Wolfy-J <wolfy.jd@gmail.com>
2025-02-16 00:19:14 -0500

    - app not exiting

---
+++ app/btea/discord/app.lua
@@ -0,0 +1,127 @@
+local bapp = require("bapp")
+local discord_client = require("discord_client")
+local env = require("env")
+local DiscordUI = require("discord_ui")
+local CommandHandler = require("discord_commands")
+
+function App()
+    -- Create app with proper init commands
+    local app = bapp.new({
+        btea.commands.enter_alt_screen,
+        btea.commands.hide_cursor
+    })
+
+    -- Initialize UI
+    app.ui = DiscordUI.new(app.window)
+
+    -- Initialize Discord client
+    app.client = discord_client.Client.new(env.get("DISCORD_BOT_TOKEN"))
+
+    -- Initialize command handler
+    app.command_handler = CommandHandler.new(app.client, app.ui)
+
+    -- Define key bindings
+    app.keys = bapp.create_keys({
+        quit = {
+            keys = { "ctrl+c", "q", "esc" },
+            help = { key = "^C/q/esc", desc = "quit" }
+        },
+        submit = {
+            keys = { "enter" },
+            help = { key = "enter", desc = "send message" }
+        }
+    })
+
+    -- Setup client event handlers
+    app.client
+        :on("connect", function()
+            app.ui:add_message("Connected to Discord gateway", "system")
+            app.ui:set_status("Connected")
+        end)
+        :on("disconnect", function()
+            app.ui:add_message("Disconnected from Discord", "system")
+            app.ui:set_status("Disconnected")
+        end)
+        :on("error", function(err)
+            app.ui:add_message("Error: " .. tostring(err), "system")
+            app.ui:set_status("Error")
+        end)
+        :on("ready", function(data)
+            app.ui:add_message("Bot is ready! Logged in as " .. data.user.username, "system")
+            app.ui:set_status("Ready")
+
+            if data.guilds and #data.guilds > 0 then
+                app.client.active_guild = data.guilds[1].id
+                app.command_handler:handle_command("!channels")
+            end
+        end)
+        :on("channel_change", function(channel)
+            app.ui:add_message("Now listening to #" .. channel.name, "system")
+            app.ui:set_active_channel(channel)
+        end)
+        :on("message", function(msg)
+            if not msg.author.bot then
+                app.ui:add_message(string.format("<%s> %s", msg.author.username, msg.content), "message")
+
+                if msg.content:match("^!") then
+                    app.command_handler:handle_command(msg.content)
+                end
+            end
+        end)
+
+    -- Start Discord client
+    app.client:start()
+
+    -- Update handler
+    local function update(self, msg)
+        -- Update window size if changed
+        if msg.window_size then
+            self.ui:set_window_size(msg.window_size)
+        end
+
+        -- Handle key events
+        if type(msg) == "table" and msg.type == "update" then
+            if msg.key then
+                if self.keys.quit:matches(msg) then
+                    return true -- signal quit
+                elseif self.keys.submit:matches(msg) then
+                    local value = self.ui:get_input_value()
+                    if value ~= "" then
+                        if value:match("^!") then
+                            self.command_handler:handle_command(value)
+                        else
+                            -- Send regular message
+                            local active_channel = self.client:get_active_channel()
+                            if active_channel then
+                                self.client:send_message(active_channel.id, value)
+                                self.ui:add_message(string.format("<You> %s", value), "message")
+                            else
+                                self.ui:add_message("No active channel. Use !listen <channel> first", "system")
+                            end
+                        end
+                        self.ui:clear_input()
+                    end
+                end
+            end
+        end
+
+        -- Update input and handle any commands
+        local cmd = self.ui:update(msg)
+        if cmd then self:dispatch(cmd) end
+
+        return false -- continue running
+    end
+
+    -- View rendering
+    local function view(self)
+        return self.ui:render()
+    end
+
+    -- Focus the input immediately
+    app:dispatch(app.ui:focus())
+
+    -- Run the app
+    app:run(update, view)
+end
+
+return App
