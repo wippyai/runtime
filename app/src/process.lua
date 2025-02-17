de4f2a52742fc7addc3d0af336bb62d6831d7f34
Wolfy-J <wolfy.jd@gmail.com>
2025-02-17 16:33:18 -0500

    - processes

--- app/src/process.lua
+++ app/src/process.lua
@@ -1,12 +1,18 @@
+local time = require("time")
+local json = require("json")
+
 local function run()
     local events_ch = pubsub.subscribe("@pid/events")
     local done = channel.new()
     local is_running = true
+    local tick = time.ticker("1s")
+    local tick_ch = tick:channel()
 
     while is_running do
         local result = channel.select({
             events_ch:case_receive(),
-            done:case_receive()
+            done:case_receive(),
+            tick_ch:case_receive()
         })
 
         if not result.ok then
@@ -17,19 +23,27 @@ local function run()
             break
         end
 
-        local event = result.value
-        if not event then
-            break
-        end
+        if result.channel == tick_ch then
+            print("Tick at:", time.now():format("15:04:05"))
+        else
+            local event = result.value
+            if event then
+                -- Handle shutdown event
+                if event.type == "shutdown" then
+                    is_running = false
+                    break
+                end
 
-        -- Handle shutdown event
-        if event.type == "shutdown" then
-            is_running = false
-            break
+                -- Dump event as JSON
+                local encoded = json.encode(event)
+                print("Received event:", encoded)
+            end
         end
     end
 
+    tick:stop()
     done:close()
+    events_ch:close()
 end
 
 return {
