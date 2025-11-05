6cf32756c0baf1394b91b1598589a2e501035152
Igor Dolgov <igor.dolgov@spiralscout.com>
2025-11-05 20:07:07 +0700

    Revert "fix(wippy): EE2-1954 fix permission condition to use exact match instead of substring"
    This reverts commit 7c2f1152dc48860c628438420b3e5a2c2226ca51.

--- service/policy/condition_evaluator.go
+++ service/policy/condition_evaluator.go
@@ -386,9 +386,6 @@ func toString(value any) (string, bool) {
 		return strconv.FormatFloat(v, 'f', -1, 64), true
 	case bool:
 		return strconv.FormatBool(v), true
-	case []any, []string, []int:
-		// Don't convert slices to strings to avoid substring matching on array string representation
-		return "", false
 	}
 
 	return fmt.Sprintf("%v", value), true
