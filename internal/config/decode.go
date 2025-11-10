e6cad5b5da5d2c70d239838197582ff04e7ade9a
Wolfy-J <wolfy.jd@gmail.com>
2025-11-10 18:56:55 -0500

    - flush

--- internal/config/decode.go
+++ internal/config/decode.go
@@ -10,7 +10,7 @@ import (
 	"go.uber.org/zap"
 )
 
-// todo: duplicatye with component
+// todo: duplicatye with component, todo: ADD SUPPORT FOR META EXTRACTION
 // DecodeAndInitConfig decodes the configuration and initializes defaults
 func DecodeAndInitConfig[T any](dtt payload.Transcoder, entry registry.Entry) (*T, error) {
 	// Get logger from context if available
